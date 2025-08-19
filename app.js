ef356bccec2a36df5235ef9eb42da9609a073138
denisse <dlgon695@gmail.com>
2025-08-18 20:37:26 -0600

    feat: new functions to add a friend to the html list and clear input

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let friends = [];
-let newFriend;
+let listOFFriends = document.getElementById('arrayFriends');
+let arrayFriends = [];
+
+function addAFriendToTheList(arrayFriends){
+    for (let name of arrayFriends){
+        //The document.createElement() method, creates a html element specify by its tagName
+        //The tag <ul> in the html indicates that when want to put an element inside the list, has to be <li> elemets.
+        let friend = document.createElement("li");
+        //The textContent property represents the text content from a node and its descendents.
+        //It works to put text(userName) inside the node(list).
+        friend.textContent = name; 
+        //Add a node(<li>) at the end of the list as a children inside the <ul>
+        listOFFriends.appendChild(friend);
+    }
+
+}
+
+function clearInput(){
+    return document.getElementById('friend').value = "";
+}
 
 function validateInput(input){
     if (input == "") {
@@ -14,11 +32,22 @@ function validateInput(input){
 function addAFriend(){
     // The trim() method remove empty spaces at the beginin and end of the array
     let userName = document.getElementById('friend').value.trim();
-    if (validateInput(userName)){
-        friends.unshift(userName);
-        console.log(`Actual friends: ${friends}`);
-        return friends;
-    }
+
+    //If validateInput is false, ends the function
+    if (!validateInput(userName)) return;
+ 
+    arrayFriends.push(userName);
+    console.log(`Actual friends: ${arrayFriends}`);
+
+    listOFFriends.innerHTML = "";
+    addAFriendToTheList(arrayFriends);
+
+    clearInput();
 }
 
+
 newFriend = addAFriend();
+/*
+    
+
+*/
