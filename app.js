86a1614855ff471e5e52dcd5de4773099f031ba6
denisse <dlgon695@gmail.com>
2025-08-19 21:24:13 -0600

    feat(js): add functions to validate friend, generate random number, get random friend, show result and to restore

--- app.js
+++ app.js
@@ -2,6 +2,73 @@
 
 let listOFFriends = document.getElementById('arrayFriends');
 let arrayFriends = [];
+let numberRandom;
+let selectedFriend;
+let result;
+
+
+function resert(){
+    arrayFriends = [];
+    document.getElementById("arrayFriends").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
+    clearInput();
+}
+
+function valideRepetedNames(userName){
+    let alreadyExists = arrayFriends.some(function(name){
+        return name.toLowerCase() === userName.toLowerCase();
+    });
+
+    if (alreadyExists){
+        alert(`${userName} is already in the list`);
+        clearInput();
+        return false;   
+    }
+    return true;
+}
+
+function clearList(){
+    return listOFFriends.innerHTML = "";
+}
+
+function drawFriend(){
+    if(arrayFriends.length === 0){
+        alert("Add a friend first");
+        return;
+    }
+
+    numberRandom = randomIndex();
+    selectedFriend = getSelectedName(numberRandom);
+    console.log(`Index: ${numberRandom}`);
+    console.log(`Name: ${selectedFriend}`);
+
+/*     //Clear the space for result
+    
+    resultList.innerHTML = ""; */
+
+    const resultList = document.getElementById("result");
+
+    //Creates a <li> with the winner name
+    const result = document.createElement("li");
+    result.textContent = "The secret friend is: " + selectedFriend;
+    resultList.appendChild(result);    
+} 
+
+function getSelectedName(index){
+    return arrayFriends[index];
+}
+
+function randomIndex(){
+    let selectedIndex = Math.floor(Math.random()*arrayFriends.length);
+    return selectedIndex;
+}
+
+function validateArrayFriends(array){
+    if(array == []) {
+        alert("Add a friend");
+        return;
+    }
+}
 
 function addAFriendToTheList(arrayFriends){
     for (let name of arrayFriends){
@@ -36,18 +103,14 @@ function addAFriend(){
     //If validateInput is false, ends the function
     if (!validateInput(userName)) return;
  
+    if (!valideRepetedNames(userName)) return;
     arrayFriends.push(userName);
     console.log(`Actual friends: ${arrayFriends}`);
 
-    listOFFriends.innerHTML = "";
+    clearList();
     addAFriendToTheList(arrayFriends);
 
     clearInput();
+    return arrayFriends;
 }
 
-
-newFriend = addAFriend();
-/*
-    
-
-*/
